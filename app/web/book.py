85ab888c7bf701e81c1603c033b0f162609ee330
xing-6464 <xing_6464@foxmail.com>
2022-12-15 17:17:04 +0800

    代码拆分

---
+++ app/web/book.py
@@ -0,0 +1,21 @@
+from flask import jsonify
+
+from helper import is_isbn_or_key
+from yushu_book import YuShuBook
+
+
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    """
+        q: 普通关键字 或者 isbn
+        page
+    """
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+
+    # 序列化
+    return jsonify(result)
+    # return json.dumps(result), 200, { 'content-type': 'application/json' }
--- fisher.py
+++ fisher.py
@@ -1,7 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask
 
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
 
 __author__ = '星光'
 
@@ -9,23 +7,6 @@ app = Flask(__name__)
 # 导入配置文件
 # 配置文件中的变量必须大写，否则读不到
 app.config.from_object('config')
-
-@app.route('/book/search/<q>/<page>')
-def search(q, page):
-    """
-        q: 普通关键字 或者 isbn
-        page
-    """
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    else:
-        result = YuShuBook.search_by_keyword(q)
-
-    # 序列化
-    return jsonify(result)
-    # return json.dumps(result), 200, { 'content-type': 'application/json' }
-    
 
 
 if __name__ == "__main__":
