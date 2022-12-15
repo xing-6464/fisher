753af732de8a2b253a8722cf62e11ddf9cc45808
xing-6464 <xing_6464@foxmail.com>
2022-12-15 22:42:22 +0800

    request对象

--- app/web/book.py
+++ app/web/book.py
@@ -1,16 +1,20 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 from . import web
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
     """
         q: 普通关键字 或者 isbn
         page
+        ?q=isbn?page=1
     """
+    # Request Response
+    q = request.args['q']
+    page = request.args['page']
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShuBook.search_by_isbn(q)
