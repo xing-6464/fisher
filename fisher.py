fa4a47ff786fc112c1018f66565ec8c609983e9b
xing-6464 <xing_6464@foxmail.com>
2022-12-15 14:46:06 +0800

    两种注册路由的方法

--- fisher.py
+++ fisher.py
@@ -4,4 +4,10 @@ __author__ = '星光'
 
 app = Flask(__name__)
 
-app.run()
+# @app.route('/hello')
+def hello():
+    return 'Hello, QiYue'
+
+app.add_url_rule('/hello', view_func=hello)
+
+app.run(debug=True, port=9527)
