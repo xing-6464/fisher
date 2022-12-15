275d374c6a709c8ce28e4a24894158ec1b44f360
xing-6464 <xing_6464@foxmail.com>
2022-12-15 22:31:24 +0800

    蓝图

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
-    # 导入配置文件
-    # 配置文件中的变量必须大写，否则读不到
-    app.config.from_object('config')
-
+    app = Flask(__name__) # flask对象
+    app.config.from_object('config') # 导入配置文件
+    register_blueprint(app) # app上注册蓝图
+    
     return app
+
+def register_blueprint(app):
+    from app.web.book import web
+    # 把蓝图注册到app对象上
+    app.register_blueprint(web)
--- app/web/book.py
+++ app/web/book.py
@@ -2,9 +2,10 @@ from flask import jsonify
 
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
+from . import web
 
 
-@app.route('/book/search/<q>/<page>')
+@web.route('/book/search/<q>/<page>')
 def search(q, page):
     """
         q: 普通关键字 或者 isbn
@@ -19,3 +20,4 @@ def search(q, page):
     # 序列化
     return jsonify(result)
     # return json.dumps(result), 200, { 'content-type': 'application/json' }
+    
