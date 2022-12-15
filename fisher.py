0a4cd45177497bfc339fb5e08ccdf3c6292b11c0
xing-6464 <xing_6464@foxmail.com>
2022-12-15 15:29:38 +0800

    搜索关键字-isbn

--- fisher.py
+++ fisher.py
@@ -7,17 +7,24 @@ app = Flask(__name__)
 # 配置文件中的变量必须大写，否则读不到
 app.config.from_object('config')
 
-@app.route('/hello')
-def hello():
-    # status code 200, 404, 301
-    # content-type http headers
-    # content-type = text/html
-    # Response
-    headers = {
-        'content-type': 'text/plain'
-    }
-    # response = make_response('<html></html>', 404)
-    # response.headers = headers
-    return '<html></html>', 404, headers
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    """
+        q: 普通关键字 或者 isbn
+        page
+    """
+    # isbn isbn13 13个0到9的数字组成
+    # isbn10 10个0到9数字组成，含有一些‘ - ’
+    isbn_or_key = 'key'
+    # 判断isbn13
+    if len(q) == 13 and q.isdigit():
+        isbn_or_key = 'isbn'
+    
+    short_q = q.replace('-', '')
+    # 判断isbn10
+    if '-' in q and short_q == 10 and short_q.isdigit():
+        isbn_or_key = 'isbn'
+    pass
+
 
 app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=9527)
