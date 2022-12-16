3acad1083ec59a0c4416c96a4cf892add039af1f
xing-6464 <xing_6464@foxmail.com>
2022-12-16 10:50:20 +0800

    将模型映射到数据库中

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
+from app.models.book import db
+
 def create_app():
     app = Flask(__name__) # flask对象
     app.config.from_object('app.secure') # 导入配置文件
     app.config.from_object('app.setting') # 导入配置文件
     register_blueprint(app) # app上注册蓝图
     
+    db.init_app(app) # 使用插件
+    db.create_all()
     return app
 
 def register_blueprint(app):
--- app/secure.py
+++ app/secure.py
@@ -1,1 +1,4 @@
 DEBUG = True
+
+# 数据库配置
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:pdx646464@localhost:3306/fisher'
