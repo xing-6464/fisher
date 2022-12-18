9a6c12412d9fdde738d0746bed7c4ecc088ab014
xing-6464 <xing_6464@foxmail.com>
2022-12-18 14:24:27 +0800

    实现书籍详情页面

--- app/web/book.py
+++ app/web/book.py
@@ -4,7 +4,7 @@ import json
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookCollection
+from app.view_models.book import BookCollection, BookViewModel
 from . import web
 
 
@@ -39,4 +39,7 @@ def search():
 
 @web.route('/book/<isbn>/detail')
 def book_detail(isbn):
-    pass
+    yushu_book = YuShuBook()
+    yushu_book.search_by_isbn(isbn)
+    book = BookViewModel(yushu_book.first)
+    return render_template('book_detail.html', book=book, wishes=[], gifts=[])
