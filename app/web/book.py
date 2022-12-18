4e4a4c95377a73062e0ab79159dadca34de1ed16
xing-6464 <xing_6464@foxmail.com>
2022-12-18 09:47:20 +0800

    从json序列化看代码解释权反转

--- app/web/book.py
+++ app/web/book.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request
+import json
 
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
@@ -17,23 +18,22 @@ def search():
     # 验证层
     form = SearchForm(request.args)
     books = BookCollection()
+
     # 参数验证成功
     if form.validate():
-
         q = form.q.data.strip()
         page = form.page.data
-
         isbn_or_key = is_isbn_or_key(q)
         yushu_book = YuShuBook()
+
         if isbn_or_key == 'isbn':
             yushu_book.search_by_isbn(q)
         else:
             yushu_book.search_by_keyword(q, page)
 
         books.fill(yushu_book, q)
-        return jsonify(books)
+        return json.dumps(books, default=lambda o: o.__dict__)
     else:
         return jsonify(form.errors)
     # 序列化
     # return json.dumps(result), 200, { 'content-type': 'application/json' }
-    
