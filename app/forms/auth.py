c62023a33a775167f7360fd4185fe5f630042e3a
xing-6464 <xing_6464@foxmail.com>
2022-12-18 18:14:29 +0800

    redirect重定向

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,7 +1,21 @@
-from wtforms import Form, StringField, PasswordField 
+from wtforms import Form, StringField, PasswordField, ValidationError
 from wtforms.validators import Length, DataRequired, Email
+
+from app.models.user import User
 
 class RegisterForm(Form):
     email = StringField(validators=[DataRequired(), Length(8, 64), Email(message='电子邮箱不符合规范')])
     password = PasswordField(validators=[DataRequired(message='密码不能为空，请输入你的密码'), Length(6, 32)])
     nickname = StringField(validators=[DataRequired(), Length(2, 10, message='昵称至少需要两个字符，最多10个字符')])
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('电子邮箱已被注册')
+
+    def validate_nickname(self, field):
+        if User.query.filter_by(nickname=field.data).first():
+            raise ValidationError('用户名已存在')
+
+class LoginForm(Form):
+    email = StringField(validators=[DataRequired(), Length(8, 64), Email(message='电子邮箱不符合规范')])
+    password = PasswordField(validators=[DataRequired(message='密码不能为空，请输入你的密码'), Length(6, 32)])
--- app/web/auth.py
+++ app/web/auth.py
@@ -1,9 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 from . import web
 from app.models.base import db
 from app.models.user import User
-from app.forms.auth import RegisterForm
+from app.forms.auth import RegisterForm, LoginForm
 
 
 @web.route('/register', methods=['GET', 'POST'])
@@ -14,12 +14,16 @@ def register():
         user.set_attrs(form.data)
         db.session.add(user)
         db.session.commit()
+        redirect(url_for('web.login'))
     return render_template('auth/register.html', form=form)
 
 
 @web.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        pass
+    return render_template('auth/login.html', form=form)
 
 
 @web.route('/reset/password', methods=['GET', 'POST'])
